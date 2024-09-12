c21e01cbf28cb3fc34170a5725089b2adb07b98a
ramirokaffo <ramirokaffo>
2024-09-12 20:35:35 +0100

    Api animation

--- animation/js/script.js
+++ animation/js/script.js
@@ -29,7 +29,7 @@ function createCustomAnimations(element) {
   }
 
 document.addEventListener("DOMContentLoaded", (event) => {
-    gsap.registerPlugin(Draggable, Flip, InertiaPlugin);
+    gsap.registerPlugin(Draggable);
       
 
 
@@ -41,27 +41,27 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
 
 
-const squares = gsap.utils.toArray(".square");
+// const squares = gsap.utils.toArray(".square");
 
 
-function doFlip() {
-  // Get the initial state
-  const state = Flip.getState(squares);
+// function doFlip() {
+//   // Get the initial state
+//   const state = Flip.getState(squares);
   
-  // Make DOM or styling changes (swap the squares in our case)
-  swap(squares);
+//   // Make DOM or styling changes (swap the squares in our case)
+//   swap(squares);
   
-  // Animate from the initial state to the end state
-  Flip.from(state, {duration: 2, ease: "power1.inOut"});
-}
+//   // Animate from the initial state to the end state
+//   Flip.from(state, {duration: 2, ease: "power1.inOut"});
+// }
 
-// Given an Array of two siblings, append the one that's first so it's last (swap)
-function swap([a, b]) {
-  a.parentNode.children[0] === a ? a.parentNode.appendChild(a) : a.parentNode.appendChild(b);
-}
+// // Given an Array of two siblings, append the one that's first so it's last (swap)
+// function swap([a, b]) {
+//   a.parentNode.children[0] === a ? a.parentNode.appendChild(a) : a.parentNode.appendChild(b);
+// }
 
-// click to flip
-document.querySelector(".button").addEventListener("click", doFlip);
+// // click to flip
+// document.querySelector(".button").addEventListener("click", doFlip);
 
 
       createCustomAnimations(myBox);
