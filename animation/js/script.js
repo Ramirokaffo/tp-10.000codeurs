9303504f687bb464c37faadec6c6aa5f645043c6
ramirokaffo <ramirokaffo>
2024-09-14 10:15:28 +0100

    Correction de height et width

--- animation/js/script.js
+++ animation/js/script.js
@@ -1,6 +1,8 @@
 
 function genererCouleurAleatoire() {
     // Les caractères hexadécimaux possibles
+
+    const colorText =  document.getElementById("color-text");
     const caracteresHexa = '0123456789ABCDEF';
     let couleur = '#';
   
@@ -8,7 +10,7 @@ function genererCouleurAleatoire() {
     for (let i = 0; i < 6; i++) {
       couleur += caracteresHexa[Math.floor(Math.random() * 16)];
     }
-  
+    colorText.innerText = couleur;
     return couleur;
   }
 
@@ -19,49 +21,30 @@ function createCustomAnimations(element) {
       gsap.to(element, { scale: "+=0.2", duration: 0.3 });
     };
   
+    const handleMouseLeave = () => {
+    const colorText =  document.getElementById("color-text");
+    colorText.innerText = "Hors de la boite";
+    };
+  
+  
     const handleMouseOver = () => {
       gsap.to(element, { backgroundColor: genererCouleurAleatoire(), duration: 0.3 });
     };
   
     element.addEventListener('dblclick', handleDoubleClick);
     element.addEventListener('mouseover', handleMouseOver);
+    element.addEventListener('mouseleave', handleMouseLeave);
 
   }
 
 document.addEventListener("DOMContentLoaded", (event) => {
     gsap.registerPlugin(Draggable);
       
-
-
       const myBox = document.getElementById('myBox');
         Draggable.create("#myBoxRot", {type: "rotation",
             inertia: true});
 
         Draggable.create("#dragAnim", {bounds: "body"});
-
-
-
-// const squares = gsap.utils.toArray(".square");
-
-
-// function doFlip() {
-//   // Get the initial state
-//   const state = Flip.getState(squares);
-  
-//   // Make DOM or styling changes (swap the squares in our case)
-//   swap(squares);
-  
-//   // Animate from the initial state to the end state
-//   Flip.from(state, {duration: 2, ease: "power1.inOut"});
-// }
-
-// // Given an Array of two siblings, append the one that's first so it's last (swap)
-// function swap([a, b]) {
-//   a.parentNode.children[0] === a ? a.parentNode.appendChild(a) : a.parentNode.appendChild(b);
-// }
-
-// // click to flip
-// document.querySelector(".button").addEventListener("click", doFlip);
 
 
       createCustomAnimations(myBox);
